ad5cf66f46d35d73dd56adc718f3cd0f14f3716c
WhskyRbbt <WhskyRbbt@Jonathans-MacBook-Air.local>
2020-03-05 18:56:16 -0800

    creating update and delete

--- controllers/characters.js
+++ controllers/characters.js
@@ -4,7 +4,8 @@ const Character = require('../models/character');
 module.exports = {
     new: newChar,
     create,
-    viewChars
+    viewChars,
+    edit
     // update
     //index
 };
@@ -35,6 +36,7 @@ function viewChars(req, res) {
     Character.find({user: req.user._id}, (err, chars) => {
         res.render('characters/view', {
             chars, 
+            user: req.user,
             name: req.user.name,
             title: "View Characters"
         })
@@ -44,3 +46,22 @@ function viewChars(req, res) {
     // function update(req,res) {
     //     console.log("this is to be upatded = " + req.body);
     // }
+function edit(req,res) {
+    Character.findById(req.params.id, (function (err, character) {
+        res.render("characters/edit", {
+            title: "edit character",
+            user: req.user,
+            name: character.name,
+            gender: character.gender,
+            race: character.race,
+            focus: character.focus,
+            role: character.role,
+            id: character.id
+        })
+
+    }))
+};
+    function update(req, res) {
+        console.log(req.body);
+        // Character.findByIdAndUpdate()
+    }
--- routes/characters.js
+++ routes/characters.js
@@ -8,8 +8,9 @@ const charactersCtrl = require('../controllers/characters');
 router.get('/characters/new', charactersCtrl.new);
 
 router.get('/characters/view', charactersCtrl.viewChars)
-
+router.get('/users/:id/characters/:id/edit', charactersCtrl.edit)
 router.post('/users/:id/characters', charactersCtrl.create);
+router.put('/users/:id/characters/:id/edit', charactersCtrl.edit)
 
 
 
